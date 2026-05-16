0739eeb80dbfd002285a67f9c1433d4b2fcd50d5
dino0908 <dinokhaw@yahoo.com.sg>
2026-05-16 21:04:58 +0800

    feat: UI

--- client/src/components/LeftPanel.tsx
+++ client/src/components/LeftPanel.tsx
@@ -10,6 +10,7 @@ interface LeftPanelProps {
   onSearch: (value: string) => void;
   error?: Error | null;
   isLoading?: boolean;
+  searchQuery: string
 }
 function LeftPanel({
   title,
@@ -18,6 +19,7 @@ function LeftPanel({
   onSearch,
   error,
   isLoading,
+  searchQuery
 }: LeftPanelProps) {
   return (
     <Box
@@ -58,6 +60,7 @@ function LeftPanel({
           placeholder={searchPlaceholder}
           onSearch={onSearch}
           isLoading={isLoading}
+          value={searchQuery}
         />
         {error && (
           <Box
